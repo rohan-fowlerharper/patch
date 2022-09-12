842c18ff339afb3facf74c3d8ca1fe03d49d7934
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-09-12 15:05:32 +1200

    add tailwind 💨

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>Patch</div>
+  return (
+    <div className='flex justify-center mt-4'>
+      <div className='bg-violet-500 w-20 text-center'>
+        <h1 className='text-mono text-violet-50'>Patch</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App
